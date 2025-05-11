6fa6f983ad27ac2be36be4284e20b9dc9e281d55
Forwall100 <johndoe@example.com>
2025-05-11 20:13:02 +0700

    change install list

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo yay -S zen-browser-bin neovim ranger vscodium-bin spotify flameshot wireguird bluez bluez-utils bluetui mpv ghostty ttf-firacode zsh uv telegram-desktop-bin
+sudo yay -S zen-browser-bin neovim ranger vscodium-bin spotify flameshot bluez bluez-utils bluetui mpv ghostty ttf-firacode zsh uv telegram-desktop-bin
 sudo systemctl enable bluetooth.service
 chsh -s /usr/bin/zsh
